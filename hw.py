8e9c4d1a8e43f24bbee8c7e9b9235a9d171a9957
PavelDT <paveldt2@gmail.com>
2017-04-11 03:19:13 -0700

    python test

---
+++ hw.py
@@ -0,0 +1,13 @@
+#!/c/python3
+print("hellow world in text file")
+
+# defines a integer variable called number
+# number = 7
+# print(number)
+
+#for loop require start and an end.
+for n in range(1,11):  
+    print(n)
+
+for n in range(1,11):  
+    print(n*5)
