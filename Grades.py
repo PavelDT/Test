29324720648c14d14053c114d1bc748397fa30cb
PavelDT <paveldt2@gmail.com>
2017-04-14 05:54:24 -0700

    added user input to grades.py file

--- Grades.py
+++ Grades.py
@@ -1,15 +1,12 @@
-testresult2 = 1
-testresult = 99
-if testresult > 90:
-    print("A")
-
+testresult = int(input("Candidate's score? "))
+#invalid input
+if testresult > 101 or testresult < 0:
+    print("invalid input")
+elif testresult >90:
+	print("A")
 elif testresult > 70:
     print("B")
 elif testresult > 50:
 	print("C")
 elif testresult < 50:
 	print("fail")
-elif testresult >100:
-	print("invalid input")
-elif testresult < 0:
-	print("invalid input")
