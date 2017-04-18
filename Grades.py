8d4ff272cde4d764ea29b216fbf0fce2688661c1
PavelDT <paveldt2@gmail.com>
2017-04-18 15:53:34 -0700

    refactored code in to function

--- Grades.py
+++ Grades.py
@@ -1,12 +1,30 @@
-testresult = int(input("Candidate's score? "))
-#invalid input
-if testresult > 101 or testresult < 0:
-    print("invalid input")
-elif testresult >90:
-	print("A")
-elif testresult > 70:
-    print("B")
-elif testresult > 50:
-	print("C")
-elif testresult < 50:
-	print("fail")
+score = int(input("Candidate's score? "))
+
+def mark_grade(testresult):
+    feedback_message = "error in processesing grade"
+    #invalid input
+    if testresult > 101 or testresult < 0:
+        print("invalid input")
+        feedback_message = "excellent work"
+    elif testresult >90:
+        print("A")
+        feedback_message = "excellent work"
+    elif testresult > 70:
+        print("B")
+        feedback_message = "excellent work"
+    elif testresult > 50:
+        print("C")
+        feedback_message = "good work"
+    elif testresult < 50:
+        print("fail")
+        feedback_message = "needs improvement"
+    
+    return feedback_message
+
+#function chaining - func within parenthesis
+print(mark_grade(score))
+
+# variable
+# function_with_no_params()
+# function_with_1_param(variable_here)
+# function_with_2_parameters(variable_here_1, 77)
