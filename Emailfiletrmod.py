a5b834cc5758b6c5d12f4617c26fa4afa40befb5
PavelDT <paveldt2@gmail.com>
2017-04-25 11:20:30 -0700

    defined and for loop + list added to email spamm filter

--- Emailfiletrmod.py
+++ Emailfiletrmod.py
@@ -1,23 +1,24 @@
-spam_words = ["free", "deal", "$", "offer", "rich", "exclusive", "prize" ] 
-# create a list of these words in python3, example email, parsse all the wards in\
-#  the example email and check if anything from the list is contained within.
-email =  "Greetings reader \
+spam_words = ["free", "deal", "$", "offer", "rich", "exclusive", "prize"]
+
+emails = ["Greetings reader \
 you have been selected as a recipient of a free exclusive offer \
 if you would like to know more about the prize you have received \
-visit us at this web address" 
+visit us at this web address", \
+          "the meeting tonight will take place in the previously discussed venue", \
+          "Special offers on sale act now - just click the link!",\
+          "we have to discuss how we will deal with this situation - contact me asap"]
 
-email2 = "the meeting tonight will take place in the previously discussed venue"
-
-email3 = "Special offers on sale act now - just click the link!"
-
-email4 = "we have to discuss how we will deal with this situation - contact me asap"
-#print the email / do not resassign a string further down the code / changes value\
-# current_spam_word is forth with referenced to spam_words = value forth with in the line below
-#without changing its value by changing string var
-for current_spam_word in spam_words:
+def spam_logic(incoming_email):
+    detected_spam = []
     # if current_spam_word in email:
-    if email.__contains__(current_spam_word):
-        print(" contains spam word: " + current_spam_word)
+    for current_spam_word in spam_words:
+        if current_email.__contains__(current_spam_word):
+            detected_spam.append(current_spam_word)  # .lenght or .size
+    # a list containing matched spam words for the current email
+    if detected_spam.__len__() >= 2:
+        print ("this email is spam")
     else:
-        print("this email is not spam")
-#creat store in list ( before for loop) 
+        print ("this email is clean")
+
+for current_email in emails:
+    spam_logic(current_email)
