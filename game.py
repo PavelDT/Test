ba18bca2d2acc51c053491f34d7c8ba71d007ddb
PavelDT <paveldt2@gmail.com>
2017-04-27 04:59:52 -0700

    modification to game.py, created 2d grid

--- game.py
+++ game.py
@@ -3,11 +3,18 @@ import sys
 
 def quit_fn():
     print ("Exiting")
+    # exiting through while condition makes this statement redundant
     sys.exit()
 def save_fn():
     print("saved")
 def new_fn():
     print("what would you like to play")
+    board_array = ([[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]])
+
+    for itemlist in board_array:
+        print(itemlist[0], itemlist[1], itemlist[2])
 def load_fn():
     print ("loading...")
 #wrap map in a function so it can be used multiple times by the meny
@@ -32,15 +39,19 @@ for menu_option in listmenu:
 # print(menu["2"])
 # print(menu["3"])
 # print(menu["4"])
+user_input = 0
+while user_input != 4:
+    print("what would you like to do")
+    user_input = int(input("what would you like to do next"))
+    if user_input == 1:
+        new_fn()
+    elif user_input == 2:
+        save_fn()
+    elif user_input == 3:
+        load_fn()
+    elif user_input == 4:
+        quit_fn()
+    else:
+        print ("invalid input")
 
-input = int(input("what would you like to do next"))
-if input == 1:
-    new_fn()
-elif input == 2:
-    save_fn()
-elif input == 3:
-    load_fn()
-elif input == 4:
-    quit_fn()
-else:
-    print ("invalid input")
+
